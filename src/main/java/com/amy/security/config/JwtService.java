979b514b7e6f347c6f5181087e6c2dceef616665
Amanecer Cabrera <amanecer.luz.cabrera@gmail.com>
2023-11-18 02:13:21 -0300

    added jwt builder

--- src/main/java/com/amy/security/config/JwtService.java
+++ src/main/java/com/amy/security/config/JwtService.java
@@ -2,13 +2,17 @@ package com.amy.security.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import lombok.Setter;
 import lombok.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Base64;
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -23,6 +27,21 @@ public class JwtService {
     }
     public String extractUserName(String token) {
         return extractClaim(token,Claims::getSubject);
+    }
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails)
+    {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24 ))
+                .signWith(getSigningKey(), SignatureAlgorithm.ES256)
+                .compact()
+
     }
 
     private Claims extractAllClaims(String token) {
