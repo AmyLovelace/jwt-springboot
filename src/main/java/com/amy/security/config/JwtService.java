1fd7bf4fd0dee4bf336cabe253b48764271f7f7c
Amanecer Cabrera <amanecer.luz.cabrera@gmail.com>
2023-11-18 01:57:51 -0300

    Update JwtService.java

--- src/main/java/com/amy/security/config/JwtService.java
+++ src/main/java/com/amy/security/config/JwtService.java
@@ -1,13 +1,39 @@
 package com.amy.security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import lombok.Setter;
+import lombok.Value;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
+    private static final String SECRET_KEY = "199D43AA954AB1DA6AB133A6CB371";
 
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractClaim(token);
+        return claimsResolver.apply(claims);
+    }
     public String extractUserEmail(String token) {
         return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(getSigningKey())
+                .build().parseSignedClaims(token).getPayload();
+
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Base64.getDecoder().decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
