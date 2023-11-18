56405c9e606c21ee2ffd8ae1a7aca30ed7cc66f2
Amanecer Cabrera <amanecer.luz.cabrera@gmail.com>
2023-11-18 00:59:50 -0300

    added userdetails

--- src/main/java/com/amy/security/user/User.java
+++ src/main/java/com/amy/security/user/User.java
@@ -1,17 +1,25 @@
 package com.amy.security.user;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+@Entity
+@Table(name = "_user")
+public class User implements UserDetails {
 
-    private int id;
+    @Id
+    @GeneratedValue
+    private Integer id;
 
     private String firstname;
 
@@ -21,4 +29,40 @@ public class User {
 
     private String password;
 
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true; //la cuenta es ta NO expirada
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
