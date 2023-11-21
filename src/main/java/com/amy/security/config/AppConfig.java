4d8ac11eb73561c2aaeba88e4f5911bbd36bf52c
Amanecer Cabrera <amanecer.luz.cabrera@gmail.com>
2023-11-20 23:34:07 -0300

    new bean

--- src/main/java/com/amy/security/config/AppConfig.java
+++ src/main/java/com/amy/security/config/AppConfig.java
@@ -5,6 +5,8 @@ import com.amy.security.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
@@ -15,10 +17,19 @@ public class AppConfig {
     private final UserRepository userRepository;
 
     @Bean
-    public UserDetailsService UserDetailsService() {
+    public UserDetailsService userDetailsService() {
         return username -> userRepository.findByEmail(username)
                 .orElseThrow(()-> new  UsernameNotFoundException("User not found"));
     }
 
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+
+
+    }
 
 }
