4ebce20964e220e601c307f507c6950c6b7676b8
Amanecer Cabrera <amanecer.luz.cabrera@gmail.com>
2023-11-21 22:31:01 -0300

    Update AuthenticationService.java

--- src/main/java/com/amy/security/auth/AuthenticationService.java
+++ src/main/java/com/amy/security/auth/AuthenticationService.java
@@ -1,14 +1,38 @@
 package com.amy.security.auth;
 
+import com.amy.security.UserRepository;
+import com.amy.security.config.JwtService;
+import com.amy.security.user.Role;
+import com.amy.security.user.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
 
+    private PasswordEncoder passwordEncoder;
+    private UserRepository repository;
+
+    private JwtService jwtService;
+
+
+
+
     public AuthenticationResponse register(RegisterRequest request) {
-        return null;
+        var user = User.builder()
+                .firstname(request.getFirstName())
+                .lastname(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
 
