efa8fa1a948c7c809540bfeab8e76cc558174687
Amanecer Cabrera <amanecer.luz.cabrera@gmail.com>
2023-11-21 22:05:44 -0300

    controller boilerplate

---
+++ src/main/java/com/amy/security/auth/AuthenticationController.java
@@ -0,0 +1,31 @@
+package com.amy.security.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/v1/auth")
+@RestController
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request){
+
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody AuthenticationRequest request){
+        
+
+    }
+
+
+
+}
+
