3b9163b6c2b17a4c7d747377580d63ea960a7a79
Amanecer Cabrera <amanecer.luz.cabrera@gmail.com>
2023-11-21 22:52:15 -0300

    Update DemoController.java

--- src/main/java/com/amy/security/demo/DemoController.java
+++ src/main/java/com/amy/security/demo/DemoController.java
@@ -1,6 +1,22 @@
 package com.amy.security.demo;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/v1/demo-controller")
+@RestController
+@RequiredArgsConstructor
 public class DemoController {
 
-    
+    @GetMapping()
+    public ResponseEntity<String> hello(){
+        return ResponseEntity.ok("hi from a secure endpoint");
+    }
+        
+
+
 }
